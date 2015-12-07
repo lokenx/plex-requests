35de1306427b27fa089d84ab39c2c7e91922e2d9
Mikhail Delport <mikhail@delport.ca>
2015-12-06 21:18:47 -0500

    User database setup

---
+++ test/authentication.js
@@ -0,0 +1,23 @@
+// 'use strict';
+//
+// const Code = require('code');
+// const Lab = require('lab');
+// const Rewire = require('rewire');
+// const Authentication = require('../lib/plugins/authentication');
+//
+// const internals = {};
+//
+// const lab = exports.lab = Lab.script();
+// const describe = lab.experiment;
+// const expect = Code.expect;
+// const it = lab.test;
+//
+// describe('/authentication', () => {
+//
+//     it('successfully logs in valid user', (done) => {
+//         Authentication.login('admin:password', (err, res) => {
+//             console.log(err, res);
+//             done();
+//         });
+//     });
+// });
